f203d6776a6987188bf2ca3d2411e6ac595c0fe1
Gene Johnson <gjohnson@newrelic.com>
2024-11-19 16:30:18 -0800

    Update product IDs to strings and add endpoint to retrieve product by ID

--- server/api/data_store.py
+++ server/api/data_store.py
@@ -1,12 +1,12 @@
 products = [
     {
-        "id": 1,
+        "id": "1",
         "name": "servlet-container",
         "description": "contianer for servlet",
         "version": "3.0",
     },
     {
-        "id": 2,
+        "id": "2",
         "name": "ejb-container",
         "description": "contianer for ejb",
         "version": "3.0",
